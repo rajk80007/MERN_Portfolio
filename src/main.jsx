e6c958895c981234fef6ffc998b956eb25c8f6f4
rajk80007 <rajk80007@gmail.com>
2024-12-06 15:44:34 +0530

    admin  routes added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/admin/Dashboard.jsx'
+import Admin from './layouts/Admin.jsx'
+
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/admin" element={<Admin
+       />}>
+        <Route index element={<Dashboard />} />
+        <Route path="dashboard" element={<div
+          style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>Dashboard</div>} />
+      </Route> 
+    </Routes>
+  </BrowserRouter>
+
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
 )
