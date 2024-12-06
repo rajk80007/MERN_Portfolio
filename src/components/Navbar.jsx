2718ef3a91486982304bdd48b9762fbaeb05caf0
rajk80007 <rajk80007@gmail.com>
2024-12-06 16:48:46 +0530

    logo svg added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 // src/components/Navbar.js
 
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 text-white shadow-md py-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-xl font-semibold">RK</div>
+        <div className="text-xl font-semibold">
+          <Link to="/">
+          RK
+          </Link>
+          </div>
         <ul className="flex space-x-6">
           <li><a href="#about" className="hover:text-gray-400">About</a></li>
           <li><a href="#skills" className="hover:text-gray-400">Skills</a></li>
