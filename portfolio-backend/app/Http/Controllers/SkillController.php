5d3059dbd24bf8040c0fb13a803c0ac5a4c896c3
rajk80007 <rajk80007@gmail.com>
2024-12-10 19:31:45 +0530

    seddwwgf

---
+++ portfolio-backend/app/Http/Controllers/SkillController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Skill;
+use Illuminate\Http\Request;
+
+class SkillController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response(
+            [
+                "message" => "success",
+                "data" => []    
+            ]   
+            );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'percentage' => 'required|string|min:0|max:100',
+        ]);
+        Skill::create($data);
+        return response(
+            [
+                "message" => "success",
+                "data" => $data
+            ]
+            );
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
